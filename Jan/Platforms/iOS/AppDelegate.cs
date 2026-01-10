db6b5fcfb07b0a7a0e7eeff28aae21b2de2c6b67
mitch092 <stevenm7861@gmail.com>
2026-01-09 23:15:24 -0800

    Add project files.

---
+++ Jan/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Jan
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
