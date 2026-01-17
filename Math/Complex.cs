0b0272b3257409b478449d5da8f92ba632cfe3b3
mitch092 <stevenm7861@gmail.com>
2026-01-16 23:04:25 -0800

    Implemented complex numbers.

---
+++ Math/Complex.cs
@@ -0,0 +1,39 @@
+﻿using System.Numerics;
+
+namespace Math
+{
+    public readonly struct Complex :
+        IAdditionOperators<Complex, Complex, Complex>,
+        ISubtractionOperators<Complex, Complex, Complex>,
+        IMultiplyOperators<Complex, Complex, Complex>,
+        IDivisionOperators<Complex, Complex, Complex>
+    {
+        public readonly FDecimal Real;
+        public readonly FDecimal Imaginary;
+
+        public Complex(FDecimal real, FDecimal imaginary)
+        {
+            Real = real;
+            Imaginary = imaginary;
+        }
+
+        public static Complex operator +(Complex left, Complex right) => new(left.Real + right.Real, left.Imaginary + right.Imaginary);
+
+        public static Complex operator -(Complex left, Complex right) => new(left.Real - right.Real, left.Imaginary - right.Imaginary);
+
+        public static Complex operator *(Complex left, Complex right)
+        {
+            FDecimal real = left.Real * right.Real - left.Imaginary * right.Imaginary;
+            FDecimal imaginary = left.Real * right.Imaginary + left.Imaginary * right.Real;
+            return new(real, imaginary);
+        }
+
+        public static Complex operator /(Complex left, Complex right)
+        {
+            FDecimal denom = right.Real * right.Real + right.Imaginary * right.Imaginary;
+            FDecimal real = left.Real * right.Real + left.Imaginary * right.Imaginary;
+            FDecimal imaginary = left.Imaginary * right.Real - left.Real * right.Imaginary;
+            return new(real / denom, imaginary / denom);
+        }
+    }
+}
---
+++ Math/FDecimal.cs
@@ -0,0 +1,52 @@
+﻿using System.Numerics;
+
+namespace Math
+{
+    /// <summary>
+    /// This is a fixed precision decimal (base 10) number, implemented on top of C#'s BigInteger class.
+    /// For now, the scaling factor is hardcoded to 4. This means that there are 4 digits after the decimal.
+    /// </summary>
+    public readonly struct FDecimal :
+        IAdditionOperators<FDecimal, FDecimal, FDecimal>,
+        ISubtractionOperators<FDecimal, FDecimal, FDecimal>,
+        IMultiplyOperators<FDecimal, FDecimal, FDecimal>,
+        IDivisionOperators<FDecimal, FDecimal, FDecimal>
+    {
+        public const int Scale = 4;
+        public static readonly BigInteger ScaleFactor = BigInteger.Pow(10, Scale);
+        public readonly BigInteger Number;
+
+        public FDecimal(BigInteger number)
+        {
+            Number = number;
+        }
+
+        /// <summary>
+        /// This function divides BigIntegers and uses the remainder for banker's rounding.
+        /// </summary>
+        /// <param name="left"></param>
+        /// <param name="right"></param>
+        /// <returns></returns>
+        public static BigInteger RDiv(BigInteger left, BigInteger right)
+        {
+            (BigInteger q, BigInteger r) = BigInteger.DivRem(left, right);
+            if (r.IsZero) return q;
+
+            BigInteger twiceR = BigInteger.Abs(r) * 2;
+            BigInteger den = BigInteger.Abs(right);
+
+            if (twiceR < den) return q;
+            if (twiceR > den) return q + q.Sign;
+
+            return q.IsEven ? q : q + q.Sign;
+        }
+
+        public static FDecimal operator +(FDecimal left, FDecimal right) => new(left.Number + right.Number);
+
+        public static FDecimal operator -(FDecimal left, FDecimal right) => new(left.Number - right.Number);
+
+        public static FDecimal operator *(FDecimal left, FDecimal right) => new(RDiv(left.Number * right.Number, ScaleFactor));
+
+        public static FDecimal operator /(FDecimal left, FDecimal right) => new(RDiv(left.Number * ScaleFactor, right.Number));
+    }
+}
